7fc08df48b25223ab03541e847f147e6c12f1401
Shabhavi Yadav <yadavshabhavi@gmail.com>
2023-02-12 19:49:06 +0530

    I have written the code

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let str="saras";   
+ let j;
+    let res;
+    for(let i=0, j=(N-1); i<=(N/2); i++, j--){
+        if(str[i]!=str[j]){
+            res="No";
+            break;
+        }
+        
+        else{
+            res="Yes";
+        }
+    }
+    console.log(res);
